0b693aa67c414e74c3f19dc4603a2309f62831fb
JCPosso <juancamiloposso01@gmail.com>
2021-11-20 17:56:01 -0500

    commit fix

--- src/Components/RegisterUserAWS/index.js
+++ src/Components/RegisterUserAWS/index.js
@@ -1,9 +1,7 @@
 import Amplify from 'aws-amplify';
-import awsconfig from './aws-exports';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 
 const RegisterUserAWS = () => {
-    Amplify.configure(awsconfig);
     return (
         <AmplifySignOut />
     )
